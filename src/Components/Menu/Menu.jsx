0d80218b912a13d5d9de0be03911e17f35e4dd3a
Joel <joelangeltw@gmail.com>
2022-01-13 14:09:21 -0500

    menu

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,13 +1,31 @@
 import './Menu.scss'
 
+import SettingsIcon from '../../icons/SettingsIcon'
+import SaveIcon from '../../icons/SaveIcon'
+import ThemeIcon from '../../icons/ThemeIcon'
+
 export default function menu() {
   return (
     <div className='menu'>
       <ul className='menu__list'>
-        <li className='menu__item'> </li>
-        <li> </li>
-        <li> </li>
-        <li> </li>
+        <li className='menu__item'>
+          <figure>
+            <SettingsIcon />
+          </figure>
+          <p>Settings</p>
+        </li>
+        <li className='menu__item'>
+          <figure>
+            <SaveIcon />
+          </figure>
+          <p>Saved</p>
+        </li>
+        <li className='menu__item'>
+          <figure>
+            <ThemeIcon />
+          </figure>
+          <p>Theme</p>
+        </li>
       </ul>
     </div>
   )
--- src/parts/Header/Header.jsx
+++ src/parts/Header/Header.jsx
@@ -1,15 +1,29 @@
+import { useState } from 'react'
+
 import './Header.scss'
 
 import UserName from '../../Components/UserName/UserName'
 import NewIcon from '../../icons/NewIcon'
 import Toggle from '../../Components/Toggle/Toggle'
+import Menu from '../../Components/Menu/Menu'
 
 export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const handleToggle = () => {
+    function menuToggle(prevMenuOpen) {
+      return !prevMenuOpen
+    }
+    setMenuOpen(menuToggle)
+  }
+
   return (
     <header className='header'>
       <UserName />
       <NewIcon />
-      <Toggle />
+      <Toggle onclick={handleToggle} />
+
+      {<Menu open={menuOpen} />}
     </header>
   )
 }
