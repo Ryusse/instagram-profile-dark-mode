43a5db486edeca29bf86ebfb238fffc778ea4869
Joel <joelangeltw@gmail.com>
2022-01-17 14:07:50 -0500

    dropdown height fixed

---
+++ src/icons/DropdownIcon.jsx
@@ -0,0 +1,20 @@
+export default function DropdownIcon({ className }) {
+  return (
+    <svg
+      className={`${className}`}
+      width='12'
+      height='6'
+      viewBox='0 0 12 6'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M1 1L6 5L11 1'
+        stroke='#252525'
+        strokeWidth='1.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  )
+}
